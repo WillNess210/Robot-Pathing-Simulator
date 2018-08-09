fedd5ad07e0df8b499ff3a1b62650bc0eae50f28
WillNess210 <WillNess210@gmail.com>
2018-08-09 17:11:50 -0600

    Fixed file links to field images

--- frc/Field.java
+++ frc/Field.java
@@ -3,8 +3,8 @@ package frc;
 import java.io.File;
 // TODO RESEARCH ENUM TO MAKE SELECTION EASIER
 public class Field{
-	public static final File blankSrc = new File("resources/BlankWhiteFIeld.png");
-	public static final File powerUp = new File("resources/PowerupFieldObstacles.png");
+	public static final File blankSrc = new File("src/resources/BlankWhiteFIeld.png");
+	public static final File powerUp = new File("src/resources/PowerupFieldObstacles.png");
 	// TODO ADD FUNCTION TO DRAW FIELD, PARAMETER IS Graphics2D
 	public static final int imageX = 1400;
 	public static final int imageY = 690;
