28f897e6eef1d621199080735b215e87124beb2b
WillNess210 <WillNess210@gmail.com>
2018-08-12 15:48:42 -0600

    Added basic PID class,

---
+++ user/BasicPID.java
@@ -0,0 +1,21 @@
+package user;
+
+public class BasicPID{
+	double P, I, D, goal, integral, previous_error;
+	public BasicPID(double p, double i, double d, double goal) {
+		this.P = p;
+		this.I = i;
+		this.D = d;
+		this.goal = goal;
+	}
+	public double getOutput(double cur) {
+		double error = this.goal - cur;
+		this.integral += (error * .02);
+		double derivative = (error - this.previous_error) / .02;
+		this.previous_error = error;
+		return cap(this.P * error + this.I*this.integral + this.D*derivative);
+	}
+	public double cap(double a) {
+		return Math.max(Math.min(a, 1), -1);
+	}
+}
--- user/User.java
+++ user/User.java
@@ -12,20 +12,11 @@ public class User{
 	// setLeftPower(double) [-1,1], setRightPower(double) [-1,1], double getLeftEncoderDistance(), double getRightEncoderDistance(), double getGyroAngle()
 	// use these to program a awesome path planning
 	public void robotPeriodic() {
-		double cmGoal = (Field.fieldXCM/2) - (robot.robotLengthCM/2);
+		double cmGoal = (Field.fieldXCM/2) - (Robot.robotLengthCM/2);
 		double kP = 1.0/120.0;
 		double leftDif = cmGoal - robot.getLeftEncoderDistance();
 		double rightDif = cmGoal - robot.getRightEncoderDistance();
-		double angGoal = 0;
-		double aKP = 1.0/8.0;
-		double angDif = robot.getGyroAngle();
-		double leftY = cap(leftDif * kP);
-		double rightY = cap(rightDif * kP);
-		double aX = cap(angDif * aKP);
-		robot.setLeftPower(leftY - aX);
-		robot.setRightPower(rightY + aX);
-	}
-	public double cap(double a) {
-		return Math.max(Math.min(a, 1), -1);
+		robot.setLeftPower(leftDif * kP);
+		robot.setRightPower(rightDif * kP);
 	}
 }
