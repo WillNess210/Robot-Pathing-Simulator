fd0639e3cf182c638637e55887cd0a5a66082e1f
WillNess210 <WillNess210@gmail.com>
2018-08-13 12:41:06 -0600

    Revert "cleared out default User class"
    This reverts commit 1abe00ee3783b50bb50be89339ad43c685859408.

--- user/User.java
+++ user/User.java
@@ -12,6 +12,11 @@ public class User{
 	// setLeftPower(double) [-1,1], setRightPower(double) [-1,1], double getLeftEncoderDistance(), double getRightEncoderDistance(), double getGyroAngle()
 	// use these to program a awesome path planning
 	public void robotPeriodic() {
-
+		double cmGoal = (Field.fieldXCM/2) - (Robot.robotLengthCM/2);
+		double kP = 1.0/120.0;
+		double leftDif = cmGoal - robot.getLeftEncoderDistance();
+		double rightDif = cmGoal - robot.getRightEncoderDistance();
+		robot.setLeftPower(leftDif * kP);
+		robot.setRightPower(rightDif * kP);
 	}
 }
