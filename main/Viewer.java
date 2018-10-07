69f025edcdc3650f1981733d01dbdd96950e1c93
WillNess210 <WillNess210@gmail.com>
2018-10-07 16:14:16 -0600

    menu demo

--- main/Viewer.java
+++ main/Viewer.java
@@ -8,26 +8,58 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class Viewer{
+public class Viewer implements Runnable{
+	private JFrame frame;
+	private JMenuBar menuBar;
+	private JMenu runButton;
+	private JMenu fileMenu;
+	private JMenu editMenu;
+	private JMenuItem openMenuItem;
+	private JMenuItem cutMenuItem;
+	private JMenuItem copyMenuItem;
+	private JMenuItem pasteMenuItem;
 	public static void main(String[] args){
+		SwingUtilities.invokeLater(new Viewer());
+	}
+	public void run() {
 		// Creating frame & pane
-		JFrame frame = new JFrame();
-		Container pane = frame.getContentPane();
-		pane.setLayout(new BorderLayout());
-		// Creating and Configuring JPanel & JFrame
-		ViewPanel renderPanel = new ViewPanel();
-		pane.add(renderPanel, BorderLayout.CENTER);
+		frame = new JFrame();
+		// Creating menu
+		menuBar = new JMenuBar();
+		// build the run button
+		runButton = new JMenu("Run");
+	    // build the File menu
+	    fileMenu = new JMenu("File");
+	    openMenuItem = new JMenuItem("Open");
+	    fileMenu.add(openMenuItem);
+
+	    // build the Edit menu
+	    editMenu = new JMenu("Edit");
+	    cutMenuItem = new JMenuItem("Cut");
+	    copyMenuItem = new JMenuItem("Copy");
+	    pasteMenuItem = new JMenuItem("Paste");
+	    editMenu.add(cutMenuItem);
+	    editMenu.add(copyMenuItem);
+	    editMenu.add(pasteMenuItem);
+
+	    // add menus to menubar
+	    menuBar.add(runButton);
+	    menuBar.add(fileMenu);
+	    menuBar.add(editMenu);
+
+	    // put the menubar on the frame
+	    frame.setJMenuBar(menuBar);
+		// Configuring frame
 		frame.setTitle("Robot Pathing Simulator");
 		frame.setResizable(false);
 		frame.setSize(1406, 800);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// Game Loop
-		while(frame.isEnabled()){
-			
-		}
 	}
 }
